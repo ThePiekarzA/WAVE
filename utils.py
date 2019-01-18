ca1dde6d764d804658d7af17da587c53df60e6a3
Emil <emilobalski@gmail.com>
2019-01-18 23:21:20 +0100

    First steps for QRS-onset finished.

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
-def find_all(a_str, sub):
-    start = 0
-    while True:
-        start = a_str.find(sub, start)
-        if start == -1: return
-        yield start
-        start += len(sub) # use start += 1 to find overlapping matches
 
+
+def getInx(ar1, len):
+    elems = 0
+    for i in range(ar1.size):
+        if ar1[i] == 0.0:
+            elems += 1
+            if elems == len:
+                return i-len+1
+        else:
+            elems = 0
